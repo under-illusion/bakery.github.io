fefef4d84e884ffbabd2c8cccf9be268bc917302
gmy <1301278201@qq.com>
2026-04-04 22:26:10 +0800

    第一次提交

---
+++ catalog.js
@@ -0,0 +1,9 @@
+/**
+ * 商品清单：image 对应 images 文件夹里的文件名。
+ * 名称、价格、类别可按实物自行修改。
+ */
+window.__BAKERY_CATALOG__ = [
+  { image: "1.png", name: "黄油牛角包", price: "12 元 / 个", category: "面包" },
+  { image: "2.png", name: "草莓奶油蛋糕（6 寸）", price: "168 元", category: "蛋糕" },
+  { image: "3.png", name: "手工曲奇礼盒", price: "58 元 / 盒", category: "点心" },
+];
